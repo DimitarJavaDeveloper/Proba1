a9f28d5c65ae481bbc24737dfe78e4a5339c5714
unknown <dimitarbelcovski29@gmail.com>
2023-03-27 10:08:00 +0200

    Add files

--- src/ProbnaKlasa.java
+++ src/ProbnaKlasa.java
@@ -1,10 +1,28 @@
+import java.util.Scanner;
 
-public class ProbnaKlasa {
+public class Pravoagolnik {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Ova e proba za cas");
+public static void main(String[] args) {
 
-	}
+double dolzina,shirina,visina, volumen;
+
+Scanner tastatura = new Scanner(System.in);
+
+System.out.println("Programata presmetuva povrsina i volumen na pravoagolnik");
+
+System.out.println("Vnesete shirina: ");
+
+shirina = tastatura.nextDouble();
+
+System.out.print("Vnesete visina: ");
+
+ visina = tastatura.nextDouble();
+
+System.out.println("Vnesete dolzina: ");
+dolzina=tastatura.nextDouble();
+
+System.out.println("Volumenot e: "+ dolzina*visina*shirina );
+
+}
 
 }
